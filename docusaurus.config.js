193abf8ff9985ecae1fdcd1b6f1228d6154db0e2
Renzo Luque <renzoluquechino1@gmail.com>
2024-01-27 04:47:42 -0500

    Actualizacion de Social Card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/imagen-card.jpg',
+      image: 'https://acortar.link/ih10KR',
       navbar: {
         title: '',
         logo: {
